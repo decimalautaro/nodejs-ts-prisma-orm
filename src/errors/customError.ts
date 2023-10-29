f8a90c5c23324211bfb1783667b64e64f4149634
Lautaro Decima <decimalautaro@gmail.com>
2023-10-28 22:27:56 -0300

    configuring erros

---
+++ src/errors/customError.ts
@@ -0,0 +1,13 @@
+export default abstract class CustomError extends Error {
+    abstract statusCode: number;
+  
+    constructor(message: string) {
+      super(message);
+    }
+  
+    abstract serializeErrors(): {
+      message: string;
+      field?: string;
+    }[];
+  }
+  
